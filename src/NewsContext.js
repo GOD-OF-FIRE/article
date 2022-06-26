e27b937a72c12806cf4c563da489fec3888e8cc5
Kushagra Gupta <kushagra357@gmail.com>
2022-06-26 11:41:41 +0530

    Update NewsContext.js

--- src/NewsContext.js
+++ src/NewsContext.js
@@ -5,12 +5,12 @@ export const NewsContext = createContext();
 
 export const NewsContextProvider = (props) => {
   const [data, setData] = useState();
-  const apiKey="5ccde40c055844f687f0db04ef169034";
+  
 
   useEffect(() => {
     axios
       .get(
-        `https://newsapi.org/v2/everything?q=tesla&from=2022-05-26&sortBy=publishedAt&apiKey=${apiKey}`
+        `https://newsapi.org/v2/everything?q=tesla&from=2022-05-26&sortBy=publishedAt&apiKey=5ccde40c055844f687f0db04ef169034`
       )
       .then((response) => setData(response.data))
       .catch((error) => console.log(error));
