a3178e22e0a895c72606c6cf18cd6cf651d2fbfa
Kushagra Gupta <kushagra357@gmail.com>
2022-06-26 11:36:09 +0530

    Updates

---
+++ precache-manifest.e55ef2a20c54a6acba48274bb0bb4866.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "8b90abf0e70840f95d1c",
+    "url": "/article/static/css/main.9afc1e78.chunk.css"
+  },
+  {
+    "revision": "8b90abf0e70840f95d1c",
+    "url": "/article/static/js/main.8b90abf0.chunk.js"
+  },
+  {
+    "revision": "517527aa6758397229f0",
+    "url": "/article/static/js/1.517527aa.chunk.js"
+  },
+  {
+    "revision": "0d0c58afe2b559d4f404",
+    "url": "/article/static/js/2.0d0c58af.chunk.js"
+  },
+  {
+    "revision": "f4e562f0c8f48d611529",
+    "url": "/article/static/js/runtime~main.f4e562f0.js"
+  },
+  {
+    "revision": "cdf815903e8b1e93424bb84ba639c217",
+    "url": "/article/index.html"
+  }
+];
